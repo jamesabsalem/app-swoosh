b7a870f20e945e2e856b27ae8c0f0b8083b29ab8
James Absalem Lintu <ja.lintu@gmail.com>
2019-05-13 20:04:27 +0600

    lession04

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by LIN2 on 5/10/19.
+//  Copyright © 2019 LIN2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+       
+    }
+
+
+}
+
